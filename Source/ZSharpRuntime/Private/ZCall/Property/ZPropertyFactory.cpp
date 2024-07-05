9961027c7e87a76cc389a5ab6709001ca30d21df
Lin7Han <941532182@qq.com>
2024-07-05 16:38:38 +0800

    Export Containers
    1. Add flags for properties provided by factory.

--- Source/ZSharpRuntime/Private/ZCall/Property/ZPropertyFactory.cpp
+++ Source/ZSharpRuntime/Private/ZCall/Property/ZPropertyFactory.cpp
@@ -6,45 +6,61 @@
 namespace ZSharp::PropertyFactory_Private
 {
 	// NOTE: KEEP SYNC WITH IntrinsicTypeId.cs
-	static const void* GUInt8TypeId = (void*)1;
-	static const void* GUInt16TypeId = (void*)2;
-	static const void* GUInt32TypeId = (void*)3;
-	static const void* GUInt64TypeId = (void*)4;
-	static const void* GInt8TypeId = (void*)5;
-	static const void* GInt16TypeId = (void*)6;
-	static const void* GInt32TypeId = (void*)7;
-	static const void* GInt64TypeId = (void*)8;
-	static const void* GFloatTypeId = (void*)9;
-	static const void* GDoubleTypeId = (void*)10;
-	static const void* GBoolTypeId = (void*)11;
+	static const void* GUInt8TypeId = reinterpret_cast<void*>(1);
+	static const void* GUInt16TypeId = reinterpret_cast<void*>(2);
+	static const void* GUInt32TypeId = reinterpret_cast<void*>(3);
+	static const void* GUInt64TypeId = reinterpret_cast<void*>(4);
+	static const void* GInt8TypeId = reinterpret_cast<void*>(5);
+	static const void* GInt16TypeId = reinterpret_cast<void*>(6);
+	static const void* GInt32TypeId = reinterpret_cast<void*>(7);
+	static const void* GInt64TypeId = reinterpret_cast<void*>(8);
+	static const void* GFloatTypeId = reinterpret_cast<void*>(9);
+	static const void* GDoubleTypeId = reinterpret_cast<void*>(10);
+	static const void* GBoolTypeId = reinterpret_cast<void*>(11);
 
-	static const void* GStringTypeId = (void*)12;
-	static const void* GNameTypeId = (void*)13;
-	static const void* GTextTypeId = (void*)14;
+	static const void* GStringTypeId = reinterpret_cast<void*>(12);
+	static const void* GNameTypeId = reinterpret_cast<void*>(13);
+	static const void* GTextTypeId = reinterpret_cast<void*>(14);
 
-	static const void* GWeakObjectPtrTypeId = (void*)15;
-	static const void* GSoftObjectPtrTypeId = (void*)16;
-	static const void* GLazyObjectPtrTypeId = (void*)17;
-	static const void* GSoftClassPtrTypeId = (void*)18;
-	static const void* GScriptInterfaceTypeId = (void*)19;
+	static const void* GWeakObjectPtrTypeId = reinterpret_cast<void*>(15);
+	static const void* GSoftObjectPtrTypeId = reinterpret_cast<void*>(16);
+	static const void* GLazyObjectPtrTypeId = reinterpret_cast<void*>(17);
+	static const void* GSoftClassPtrTypeId = reinterpret_cast<void*>(18);
+	static const void* GScriptInterfaceTypeId = reinterpret_cast<void*>(19);
+
+	template <std::derived_from<FProperty> T>
+	T* Create(EPropertyFlags flags)
+	{
+		auto prop = new T(nullptr, "", RF_Public | RF_MarkAsNative | RF_Transient);
+		prop->SetPropertyFlags(flags);
+		return prop;
+	}
+
+	static constexpr EPropertyFlags PrimitiveFlags = CPF_ZeroConstructor | CPF_IsPlainOldData | CPF_NoDestructor | CPF_HasGetValueTypeHash;
 
 	static TMap<const void*, TFunction<FProperty*(const FZPropertyDesc&)>> FactoryMap
 	{
-		{ GUInt8TypeId, [](const FZPropertyDesc&){ return new FByteProperty(nullptr, "", RF_Transient); } },
-		{ GUInt16TypeId, [](const FZPropertyDesc&){ return new FUInt16Property(nullptr, "", RF_Transient); } },
-		{ GUInt32TypeId, [](const FZPropertyDesc&){ return new FUInt32Property(nullptr, "", RF_Transient); } },
-		{ GUInt64TypeId, [](const FZPropertyDesc&){ return new FUInt64Property(nullptr, "", RF_Transient); } },
-		{ GInt8TypeId, [](const FZPropertyDesc&){ return new FInt8Property(nullptr, "", RF_Transient); } },
-		{ GInt16TypeId, [](const FZPropertyDesc&){ return new FInt16Property(nullptr, "", RF_Transient); } },
-		{ GInt32TypeId, [](const FZPropertyDesc&){ return new FIntProperty(nullptr, "", RF_Transient); } },
-		{ GInt64TypeId, [](const FZPropertyDesc&){ return new FInt64Property(nullptr, "", RF_Transient); } },
-		{ GFloatTypeId, [](const FZPropertyDesc&){ return new FFloatProperty(nullptr, "", RF_Transient); } },
-		{ GDoubleTypeId, [](const FZPropertyDesc&){ return new FDoubleProperty(nullptr, "", RF_Transient); } },
-		{ GBoolTypeId, [](const FZPropertyDesc&){ return new FBoolProperty(nullptr, "", RF_Transient); } },
+		{ GUInt8TypeId, [](const FZPropertyDesc&){ return Create<FByteProperty>(PrimitiveFlags); } },
+		{ GUInt16TypeId, [](const FZPropertyDesc&){ return Create<FUInt16Property>(PrimitiveFlags); } },
+		{ GUInt32TypeId, [](const FZPropertyDesc&){ return Create<FUInt32Property>(PrimitiveFlags); } },
+		{ GUInt64TypeId, [](const FZPropertyDesc&){ return Create<FUInt64Property>(PrimitiveFlags); } },
+		{ GInt8TypeId, [](const FZPropertyDesc&){ return Create<FInt8Property>(PrimitiveFlags); } },
+		{ GInt16TypeId, [](const FZPropertyDesc&){ return Create<FInt16Property>(PrimitiveFlags); } },
+		{ GInt32TypeId, [](const FZPropertyDesc&){ return Create<FIntProperty>(PrimitiveFlags); } },
+		{ GInt64TypeId, [](const FZPropertyDesc&){ return Create<FInt64Property>(PrimitiveFlags); } },
+		{ GFloatTypeId, [](const FZPropertyDesc&){ return Create<FFloatProperty>(PrimitiveFlags); } },
+		{ GDoubleTypeId, [](const FZPropertyDesc&){ return Create<FDoubleProperty>(PrimitiveFlags); } },
+		{ GBoolTypeId, [](const FZPropertyDesc&){ return Create<FBoolProperty>(PrimitiveFlags); } },
 		
-		{ GStringTypeId, [](const FZPropertyDesc&){ return new FStrProperty(nullptr, "", RF_Transient); } },
-		{ GNameTypeId, [](const FZPropertyDesc&){ return new FNameProperty(nullptr, "", RF_Transient); } },
-		{ GTextTypeId, [](const FZPropertyDesc&){ return new FTextProperty(nullptr, "", RF_Transient); } },
+		{ GStringTypeId, [](const FZPropertyDesc&){ return Create<FStrProperty>(CPF_ZeroConstructor | CPF_HasGetValueTypeHash); } },
+		{ GNameTypeId, [](const FZPropertyDesc&){ return Create<FNameProperty>(PrimitiveFlags); } },
+		{ GTextTypeId, [](const FZPropertyDesc&){ return Create<FTextProperty>(CPF_None); } },
+
+		{ GWeakObjectPtrTypeId, [](const FZPropertyDesc&){ return Create<FWeakObjectProperty>(PrimitiveFlags | CPF_UObjectWrapper); } },
+		{ GSoftObjectPtrTypeId, [](const FZPropertyDesc&){ return Create<FSoftObjectProperty>(CPF_UObjectWrapper | CPF_HasGetValueTypeHash); } },
+		{ GLazyObjectPtrTypeId, [](const FZPropertyDesc&){ return Create<FLazyObjectProperty>(CPF_IsPlainOldData | CPF_NoDestructor | CPF_UObjectWrapper | CPF_HasGetValueTypeHash); } },
+		{ GSoftClassPtrTypeId, [](const FZPropertyDesc&){ return Create<FSoftClassProperty>(CPF_UObjectWrapper | CPF_HasGetValueTypeHash); } },
+		{ GScriptInterfaceTypeId, [](const FZPropertyDesc&){ return Create<FInterfaceProperty>(PrimitiveFlags | CPF_UObjectWrapper); } },
 	};
 }
 
@@ -58,13 +74,13 @@ FProperty* ZSharp::FZPropertyFactory::Create(const FZPropertyDesc& desc)
 	UStruct* strct = static_cast<UStruct*>(desc.Descriptor);
 	if (auto cls = Cast<UClass>(strct))
 	{
-		auto prop = new FObjectProperty(nullptr, "", RF_Transient);
+		auto prop = PropertyFactory_Private::Create<FObjectProperty>(CPF_None);
 		prop->PropertyClass = cls;
 		return prop;
 	}
 	else if (auto scriptStruct = Cast<UScriptStruct>(strct))
 	{
-		auto prop = new FStructProperty(nullptr, "", RF_Transient);
+		auto prop = PropertyFactory_Private::Create<FStructProperty>(CPF_None);
 		prop->ElementSize = scriptStruct->GetStructureSize();
 		prop->Struct = scriptStruct;
 		return prop;

b4909df055859926353d3f02d82e904b6e3f577c
Lin7Han <941532182@qq.com>
2024-07-13 08:46:04 +0800

    Commit missing file UnrealFieldPathAttribute.cs

---
+++ Source/Managed/ZeroGames.ZSharp.UnrealEngine/Source/UnrealFieldPathAttribute.cs
@@ -0,0 +1,12 @@
+﻿// Copyright Zero Games. All Rights Reserved.
+
+namespace ZeroGames.ZSharp.UnrealEngine;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum, Inherited = false)]
+public class UnrealFieldPathAttribute : Attribute
+{
+	public UnrealFieldPathAttribute(string path) => Path = path;
+	public string Path { get; }
+}
+
+

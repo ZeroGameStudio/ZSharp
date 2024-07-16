7fe2a936d0a80612214b9389141a95e32b464fd0
Lin7Han <941532182@qq.com>
2024-07-16 14:31:51 +0800

    Update Build assembly & Add missing file StringEx.Text.cs

---
+++ Source/Managed/ZeroGames.ZSharp.Build/Source/StringEx.Text.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Zero Games. All Rights Reserved.
+
+using System.Text;
+
+namespace ZeroGames.ZSharp.Build;
+
+public static class StringEx_Text
+{
+
+	public static string Indent(this string @this, int32 count = 1, string indentUnit = "\t")
+	{
+		StringBuilder unitSb = new();
+		for (int32 i = 0; i < count; ++i)
+		{
+			unitSb.Append(indentUnit);
+		}
+
+		string indent = unitSb.ToString();
+		string newlineindent = '\n' + indent;
+
+		return @this.Insert(0, indent).Replace("\n", newlineindent);
+	}
+	
+}
+
+

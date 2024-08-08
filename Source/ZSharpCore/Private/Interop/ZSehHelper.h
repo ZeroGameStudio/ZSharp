e39bb15f06d2d145b10424f858fbe0f859759bbc
Lin7Han <941532182@qq.com>
2024-08-08 15:45:10 +0800

    Update ZSehHelper.h

--- Source/ZSharpCore/Private/Interop/ZSehHelper.h
+++ Source/ZSharpCore/Private/Interop/ZSehHelper.h
@@ -2,8 +2,11 @@
 
 #pragma once
 
+#if PLATFORM_SEH_EXCEPTIONS_DISABLED
+#define GUARDED_INVOKE(Try, Catch) return Try;
+#else
 #include "HAL/ExceptionHandling.h"
-
+	
 namespace ZSharp::ZSehHelper_Private
 {
 	inline int32 ReportCrash(Windows::LPEXCEPTION_POINTERS ExceptionInfo)
@@ -14,13 +17,7 @@ namespace ZSharp::ZSehHelper_Private
 	}
 }
 
-#if PLATFORM_SEH_EXCEPTIONS_DISABLED
-	#define GUARDED_INVOKE(Try, Catch) return Try;
-#else
-	#define GUARDED_INVOKE(Try, Catch) __try { return Try; } __except(FPlatformMisc::GetCrashHandlingType() == ECrashHandlingType::Default ? (ZSharp::ZSehHelper_Private::ReportCrash(GetExceptionInformation()), EXCEPTION_CONTINUE_SEARCH) : EXCEPTION_CONTINUE_SEARCH) { return Catch; }
+#define GUARDED_INVOKE(Try, Catch) __try { return Try; } __except(FPlatformMisc::GetCrashHandlingType() == ECrashHandlingType::Default ? (ZSharp::ZSehHelper_Private::ReportCrash(GetExceptionInformation()), EXCEPTION_CONTINUE_SEARCH) : EXCEPTION_CONTINUE_SEARCH) { return Catch; }
 #endif
-
-
-	
 
 

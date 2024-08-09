b4d8be72e865635e2ea1f5a07c99efc500fb1948
Lin7Han <941532182@qq.com>
2024-08-09 18:15:32 +0800

    Update ZMulticastSparseDelegatePropertyVisitor.cpp

--- Source/ZSharpRuntime/Private/Reflection/Property/Delegate/ZMulticastSparseDelegatePropertyVisitor.cpp
+++ Source/ZSharpRuntime/Private/Reflection/Property/Delegate/ZMulticastSparseDelegatePropertyVisitor.cpp
@@ -3,6 +3,7 @@
 
 #include "ZMulticastSparseDelegatePropertyVisitor.h"
 
+#include "ZSharpRuntimeLogChannels.h"
 #include "ALC/IZMasterAssemblyLoadContext.h"
 #include "CLR/IZSharpClr.h"
 #include "ZCall/ZCallBuffer.h"
@@ -10,18 +11,7 @@
 
 void ZSharp::FZMulticastSparseDelegatePropertyVisitor::GetValue(const void* src, FZCallBufferSlot& dest) const
 {
-	checkNoEntry();
-	
-	// const FZSelfDescriptiveMulticastSparseScriptDelegate* sdsd = IZSharpClr::Get().GetMasterAlc()->GetConjugateRegistry<FZConjugateRegistry_MulticastSparseDelegate>().Conjugate(dest.ReadConjugate());
-	// if (!sdsd)
-	// {
-	// 	dest.WriteConjugate(IZSharpClr::Get().GetMasterAlc()->GetConjugateRegistry<FZConjugateRegistry_MulticastSparseDelegate>().Conjugate(CastChecked<USparseDelegateFunction>(UnderlyingDelegateProperty->SignatureFunction), [src, this](const FZSelfDescriptiveMulticastSparseScriptDelegate& sdsd){ UnderlyingProperty->CopySingleValue(sdsd.GetUnderlyingInstance(), src); }));
-	// }
-	// else
-	// {
-	// 	check(sdsd->GetDescriptor() == UnderlyingDelegateProperty->SignatureFunction);
-	// 	UnderlyingProperty->CopySingleValue(sdsd->GetUnderlyingInstance(), src);
-	// }
+	UE_LOG(LogZSharpRuntime, Fatal, TEXT("Getting copy of sparse delegate is NEVER allowed!!!"));
 }
 
 void ZSharp::FZMulticastSparseDelegatePropertyVisitor::GetRef(const void* src, FZCallBufferSlot& dest) const
@@ -31,16 +21,7 @@ void ZSharp::FZMulticastSparseDelegatePropertyVisitor::GetRef(const void* src, F
 
 void ZSharp::FZMulticastSparseDelegatePropertyVisitor::SetValue(void* dest, const FZCallBufferSlot& src) const
 {
-	checkNoEntry();
-	
-	// const FZSelfDescriptiveMulticastSparseScriptDelegate* sdsd = IZSharpClr::Get().GetMasterAlc()->GetConjugateRegistry<FZConjugateRegistry_MulticastSparseDelegate>().Conjugate(src.ReadConjugate());
-	// if (!sdsd)
-	// {
-	// 	UnderlyingProperty->InitializeValue(dest);
-	// }
-	//
-	// check(sdsd->GetDescriptor() == UnderlyingDelegateProperty->SignatureFunction);
-	// UnderlyingProperty->CopySingleValue(dest, sdsd->GetUnderlyingInstance());
+	UE_LOG(LogZSharpRuntime, Fatal, TEXT("Setting value of sparse delegate is NEVER allowed!!!"));
 }
 
 

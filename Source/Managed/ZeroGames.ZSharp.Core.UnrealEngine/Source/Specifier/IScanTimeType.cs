d829dab406aff1ff1859cecc9355746d299a1436
Lin7Han <941532182@qq.com>
2024-10-12 11:17:29 +0800

    Update IScanTimeType.cs

--- Source/Managed/ZeroGames.ZSharp.Core.UnrealEngine/Source/Specifier/IScanTimeType.cs
+++ Source/Managed/ZeroGames.ZSharp.Core.UnrealEngine/Source/Specifier/IScanTimeType.cs
@@ -3,7 +3,7 @@
 namespace ZeroGames.ZSharp.Core.UnrealEngine.Specifier;
 
 /// <summary>
-/// An opacity fallback runtime type object used on scanner generated System.Attribute object.
+/// A transparent fallback runtime type object used on scanner generated System.Attribute object.
 /// This is because at scan-time, the actual System.Type object may not in-memory.
 /// This interface provides no information, but scanner should know it's actual type and should use a dynamic cast.
 /// </summary>

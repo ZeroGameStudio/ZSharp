99f67d0943e8a104a3ac57d7bdcfff493eb03bd7
Lin7Han <941532182@qq.com>
2024-10-26 15:06:46 +0800

    Update AsyncSettings.cs

--- Source/Managed/ZeroGames.ZSharp.Core.Async/Source/Misc/AsyncSettings.cs
+++ Source/Managed/ZeroGames.ZSharp.Core.Async/Source/Misc/AsyncSettings.cs
@@ -5,8 +5,8 @@ namespace ZeroGames.ZSharp.Core.Async;
 public static class AsyncSettings
 {
 
-	public static bool UseZSharpSynchronizationContext => !GConfig.TryGetBool(BaseIniName, Section, nameof(UseZSharpSynchronizationContext), out var value) || value.Value;
-	public static bool ForceAsyncZeroTaskMethodContinueOnGameThread => !GConfig.TryGetBool(BaseIniName, Section, nameof(ForceAsyncZeroTaskMethodContinueOnGameThread), out var value) || value.Value;
+	public static bool UseZSharpSynchronizationContext { get; } = !GConfig.TryGetBool(BaseIniName, Section, nameof(UseZSharpSynchronizationContext), out var value) || value.Value;
+	public static bool ForceAsyncZeroTaskMethodContinueOnGameThread { get; } = !GConfig.TryGetBool(BaseIniName, Section, nameof(ForceAsyncZeroTaskMethodContinueOnGameThread), out var value) || value.Value;
 	
 	private const string BaseIniName = "ZSharp";
 	private const string Section = "AsyncSettings";

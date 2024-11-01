c2c4baaa5d90e8d03a02df420aa3ee2d287853fc
Lin7Han <941532182@qq.com>
2024-11-01 16:02:32 +0800

    Optimize core comment.

--- Source/ZSharpCore/Public/Conjugate/ZDeclareConjugateRegistryMacros.h
+++ Source/ZSharpCore/Public/Conjugate/ZDeclareConjugateRegistryMacros.h
@@ -7,6 +7,14 @@
 #define ZSHARP_DECLARE_CONJUGATE_REGISTRY_COMBINE_INNER(A, B, C) A##B##C
 #define ZSHARP_DECLARE_CONJUGATE_REGISTRY_COMBINE(A, B, C) ZSHARP_DECLARE_CONJUGATE_REGISTRY_COMBINE_INNER(A, B, C)
 
+/**
+ * Declares a conjugate registry to automatically register to Master ALC.
+ * Generally you don't need to care about this
+ * because Z# has implemented necessary conjugates for interacting with Unreal Engine.
+ * 
+ * @param RegistryType Type of the conjugate registry.
+ * @param RegistryName An optional name only used to build variable name.
+ */
 #define ZSHARP_DECLARE_CONJUGATE_REGISTRY_EX(RegistryType, RegistryName) namespace ZSharp::__ZDeclareConjugateRegistry_Private { static TZDeclareConjugateRegistry<RegistryType> ZSHARP_DECLARE_CONJUGATE_REGISTRY_COMBINE(__GDeclareConjugateRegistry, RegistryName, __LINE__); }
 #define ZSHARP_DECLARE_CONJUGATE_REGISTRY(RegistryType) ZSHARP_DECLARE_CONJUGATE_REGISTRY_EX(RegistryType, RegistryType)
 
--- Source/ZSharpCore/Public/Misc/Log/ZRegisterLogCategoryMacros.h
+++ Source/ZSharpCore/Public/Misc/Log/ZRegisterLogCategoryMacros.h
@@ -7,6 +7,12 @@
 #define ZSHARP_REGISTER_LOG_CATEGORY_COMBINE_INNER(A, B, C) A##B##C
 #define ZSHARP_REGISTER_LOG_CATEGORY_COMBINE(A, B, C) ZSHARP_REGISTER_LOG_CATEGORY_COMBINE_INNER(A, B, C)
 
+/**
+ * Registers a log category to managed side.
+ * 
+ * @param Category a LogCategoryBase& to register.
+ * @param CategoryName An optional name only used to build variable name.
+ */
 #define ZSHARP_REGISTER_LOG_CATEGORY_EX(Category, CategoryName) namespace ZSharp::__ZRegisterLogCategory_Private { static FZRegisterLogCategory ZSHARP_REGISTER_LOG_CATEGORY_COMBINE(__GRegisterLogCategory, CategoryName, __LINE__) { Category }; }
 #define ZSHARP_REGISTER_LOG_CATEGORY(Category) ZSHARP_REGISTER_LOG_CATEGORY_EX(Category, Category)
 

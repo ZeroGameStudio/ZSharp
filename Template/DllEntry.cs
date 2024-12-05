1e35631f7ca3bfbf3f1f4fef62e01f79a840820b
Lin7Han <941532182@qq.com>
2024-12-05 19:59:24 +0800

    Optimize guide templates.

--- Template/DllEntry.cs
+++ Template/DllEntry.cs
@@ -1,15 +1,21 @@
 ﻿// Copyright Zero Games. All Rights Reserved.
 
+/*
+ * This tells Z# the entry point of this assembly is in Game.DllEntry class.
+ */
 [assembly: DllEntry(typeof(Game.DllEntry))]
 
 namespace Game;
 
 public class DllEntry
 {
+    /*
+     * This tells Z# it is the entry point of this assembly.
+     */
     [DllMain]
-    public static void DllMain()
+    public static void DllMain(string[] args)
     {
-        UE_LOG(LogTemp, "============= Game Assembly Loaded! ===========");
+        UE_LOG(LogTemp, $"============= Game Assembly Loaded! {string.Join("", args)} ===========");
     }
 }
 

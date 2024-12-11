8ab5c20f3030e1e03788a2cc1b65327b3960af4b
Lin7Han <941532182@qq.com>
2024-12-11 13:34:48 +0800

    Update UnrealObjectBase.cs

--- Source/Managed/ZeroGames.ZSharp.UnrealEngine/Source/CoreUObject/Object/UnrealObjectBase.cs
+++ Source/Managed/ZeroGames.ZSharp.UnrealEngine/Source/CoreUObject/Object/UnrealObjectBase.cs
@@ -6,6 +6,20 @@ namespace ZeroGames.ZSharp.UnrealEngine.CoreUObject;
 public abstract class UnrealObjectBase : UnrealExportedObjectBase
 {
 
+    public DynamicZCallResult CallUnrealFunctionEx<T>(string name, params ReadOnlySpan<object?> parameters)
+    {
+        string zcallName = $"uf:/{UnrealFieldPath}:{name}";
+        return this.ZCall(MasterAlcCache.Instance, zcallName, [ ..parameters, typeof(T) ]);
+    }
+
+    public T? CallUnrealFunction<T>(string name, params ReadOnlySpan<object?> parameters) => (T?)CallUnrealFunctionEx<T>(name, parameters)[-1].Object;
+
+    public DynamicZCallResult CallUnrealFunction(string name, params ReadOnlySpan<object?> parameters)
+    {
+        string zcallName = $"uf:/{UnrealFieldPath}:{name}";
+        return this.ZCall(MasterAlcCache.Instance, zcallName, parameters);
+    }
+
     public DynamicZCallResult ReadUnrealPropertyEx<T>(string name, int32 index)
     {
         string zcallName = $"up:/{UnrealFieldPath}:{name}";
@@ -25,20 +39,6 @@ public abstract class UnrealObjectBase : UnrealExportedObjectBase
     }
 
     public DynamicZCallResult WriteUnrealProperty<T>(string name, T value) => WriteUnrealProperty(name, 0, value);
-
-    public DynamicZCallResult CallUnrealFunctionEx<T>(string name, params ReadOnlySpan<object?> parameters)
-    {
-        string zcallName = $"uf:/{UnrealFieldPath}:{name}";
-        return this.ZCall(MasterAlcCache.Instance, zcallName, [ ..parameters, typeof(T) ]);
-    }
-
-    public T? CallUnrealFunction<T>(string name, params ReadOnlySpan<object?> parameters) => (T?)CallUnrealFunctionEx<T>(name, parameters)[-1].Object;
-
-    public DynamicZCallResult CallUnrealFunction(string name, params ReadOnlySpan<object?> parameters)
-    {
-        string zcallName = $"uf:/{UnrealFieldPath}:{name}";
-        return this.ZCall(MasterAlcCache.Instance, zcallName, parameters);
-    }
 
     protected UnrealObjectBase(IntPtr unmanaged) : base(unmanaged){}
 

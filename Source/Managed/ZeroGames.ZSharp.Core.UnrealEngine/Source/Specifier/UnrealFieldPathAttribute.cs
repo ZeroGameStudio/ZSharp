5d5e024f797321b54da320b4b028039abc5b4769
Lin7Han <941532182@qq.com>
2025-01-03 14:20:03 +0800

    Update UnrealFieldPathAttribute target.

--- Source/Managed/ZeroGames.ZSharp.Core.UnrealEngine/Source/Specifier/UnrealFieldPathAttribute.cs
+++ Source/Managed/ZeroGames.ZSharp.Core.UnrealEngine/Source/Specifier/UnrealFieldPathAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace ZeroGames.ZSharp.Core.UnrealEngine.Specifier;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum | AttributeTargets.Delegate, Inherited = false)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Enum, Inherited = false)]
 public class UnrealFieldPathAttribute(string path) : Attribute, IUnrealReflectionSpecifier
 {
 	public string Path { get; } = path;

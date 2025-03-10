90acbb656a25244d9b394a56d724ce88cb1c346a
Lin7Han <941532182@qq.com>
2025-03-10 12:43:35 +0800

    Update example templates.

--- Template/MagicCube.cs
+++ Template/MagicCube.cs
@@ -2,16 +2,15 @@
 
 /*
  * Namespace of a dynamic UClass must be <AssemblyName>.<ModuleName>.
+ * <ModuleName> can be removed if it equals the last segment of <AssemblyName>
  */
-namespace Game.${GameModuleName};
+namespace Game;
 
 /*+
  * Define a UClass that can be inherited by blueprint.
  * Place this class into map, play in editor, and you will see a rotating cube.
- * Behavior of [PropertyDefaultOverride] is same as directly set property value in C++ constructor, but only valid for UProperty.
  */
 [UClass, Blueprintable]
-[PropertyDefaultOverride(Property = "PrimaryActorTick.bCanEverTick", Default = true)]
 public partial class MagicCube : Actor
 {
 
@@ -26,33 +25,37 @@ public partial class MagicCube : Actor
 	 */
 	[UFunction, BlueprintNativeEvent, BlueprintCallable]
 	public partial void SayHello();
-	protected virtual partial void SayHello_Implementation()
-	{
-		KismetSystemLibrary.PrintString(this);
-	}
 
 	/*
 	 * Define a pure virtual UFunction, which means you can only override it in blueprint.
 	 */
 	[UFunction, BlueprintImplementableEvent, BlueprintCallable]
 	public partial void ChangeColor();
-
-	protected override void UserConstructionScript_Implementation()
-	{
-		StaticMesh.SetStaticMesh(LoadObject<StaticMesh>("/Engine/BasicShapes/Cube.Cube"));
-	}
+	
+	/*
+	 * Define a DSO as UProperty.
+	 * Behavior of [DefaultSubobject] is same as calling CreateDefaultSubobject() in C++ class constructor.
+	 * Behavior of [RootComponent] is same as calling SetRootComponent() in C++ class constructor.
+	 */
+	[UProperty, DefaultSubobject, RootComponent, VisibleAnywhere]
+	public partial StaticMeshComponent StaticMesh { get; set; }
+	
+	/*
+	 * Define a UProperty that can be edited in property editor.
+	 */
+	[UProperty(Default = 180), EditAnywhere]
+	public partial double RotationSpeed { get; set; }
 
 	/*
-	 * Override an existing BlueprintImplementableEvent ReceiveBeginPlay.
-	 * Since it is an override other than a definition, you should not add [UFunction] attribute on it.
+	 * Override 'red' constructor, which runs when native UObject is created and Master ALC reloads.
 	 */
-	protected override async void ReceiveBeginPlay_Implementation()
+	public MagicCube(IntPtr unmanaged) : base(unmanaged)
 	{
 		/*
 		 * Native object returned by function is 'red',
 		 * which means it's lifecycle is owned by C++ and can be released at any time.
 		 * So if you want to cache it in script, you should create a copy from script side.
-		 * 
+		 *
 		 * Native object created by script is 'black',
 		 * which means it's lifecycle is owned by managed garbage collector,
 		 * so it will be alive until it is not referenced by anyone.
@@ -64,20 +67,37 @@ public partial class MagicCube : Actor
 		ensure(startPos.IsRed);
 		ensure(_startPos.IsBlack);
 		ensure(_startPos == startPos);
+	}
+	
+	protected virtual partial void SayHello_Implementation()
+	{
+		KismetSystemLibrary.PrintString(this);
+	}
+	
+	protected override void UserConstructionScript_Implementation()
+	{
+		StaticMesh.SetStaticMesh(LoadObject<StaticMesh>("/Engine/BasicShapes/Cube.Cube"));
+	}
 
+	/*
+	 * Override an existing BlueprintImplementableEvent ReceiveBeginPlay.
+	 * Since it is an override other than a definition, you should not add [UFunction] attribute on it.
+	 */
+	protected override async void ReceiveBeginPlay_Implementation()
+	{
 		RotateAsync();
 
 		/*
 		 * This will suspend the function for 1s but won't block the game thread.
 		 */
-		await ZeroTask.DelaySeconds(1, Lifecycle);
+		await ZeroTask.Delay(1, Lifecycle);
 		
 		/*
 		 * This log will appear after 1s.
 		 */
 		UE_LOG(LogTemp, $"{GetPathName()} Begin Play!");
 		
-		await ZeroTask.DelaySeconds(2, Lifecycle);
+		await ZeroTask.Delay(2, Lifecycle);
 		
 		/*
 		 * This actor's lifespan is totally 1s + 2s + 2s = 5s.
@@ -104,6 +124,15 @@ public partial class MagicCube : Actor
 		double offsetZ = 100 * Math.Sin(_accumulatedTime);
 		K2_SetActorLocation(new(_startPos.X, _startPos.Y, _startPos.Z + offsetZ), false, out _, true);
 	}
+	
+	/*
+	 * Define UClass constructor, which runs only when native UObject is created.
+	 */
+	[UClassConstructor]
+	private void ConstructMagicCube()
+	{
+		PrimaryActorTick.bCanEverTick = true;
+	}
 
 	private async ZeroTask RotateAsync()
 	{
@@ -114,24 +143,10 @@ public partial class MagicCube : Actor
 	}
 
 	/*
-	 * Define a DSO as UProperty.
-	 * Behavior of [DefaultSubobject] is same as calling CreateDefaultSubobject() in C++ class constructor.
-	 * Behavior of [RootComponent] is same as calling SetRootComponent() in C++ class constructor.
-	 */
-	[UProperty, DefaultSubobject, RootComponent, VisibleAnywhere]
-	public partial StaticMeshComponent StaticMesh { get; set; }
-	
-	/*
-	 * Define a UProperty that can be edited in property editor.
-	 */
-	[UProperty(Default = 180), EditAnywhere]
-	public partial double RotationSpeed { get; set; }
-
-	/*
 	 * Z# keeps lifecycle synchronized between native object and script object,
 	 * so the script object can also hold states.
 	 */
-	private Vector _startPos = null!;
+	private Vector _startPos;
 	private float _accumulatedTime;
 
 }

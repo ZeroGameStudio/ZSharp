21e495c7d2735820bace74e171cd7d50d4e1be00
Lin7Han <941532182@qq.com>
2025-04-06 14:51:14 +0800

    Optimize lifecycle interface.

--- Script/ZeroGames.ZSharp.Core.Async/Source/Lifecycle/ILifecycleBackend.cs
+++ Script/ZeroGames.ZSharp.Core.Async/Source/Lifecycle/ILifecycleBackend.cs
@@ -2,12 +2,11 @@
 
 namespace ZeroGames.ZSharp.Core.Async;
 
-public interface ILifecycleBackend
+public interface ILifecycleBackend : ILifecycleSource
 {
 	bool IsExpired(LifecycleToken token);
 	
 	LifecycleToken Token { get; }
-	Lifecycle Lifecycle { get; }
 }
 
 
--- Script/ZeroGames.ZSharp.Core.Async/Source/Lifecycle/IReactiveLifecycleBackend.cs
+++ Script/ZeroGames.ZSharp.Core.Async/Source/Lifecycle/IReactiveLifecycleBackend.cs
@@ -2,13 +2,11 @@
 
 namespace ZeroGames.ZSharp.Core.Async;
 
-public interface IReactiveLifecycleBackend : ILifecycleBackend
+public interface IReactiveLifecycleBackend : ILifecycleBackend, IReactiveLifecycleSource
 {
 	LifecycleExpiredRegistration RegisterOnExpired(Action callback, LifecycleToken token);
 	LifecycleExpiredRegistration RegisterOnExpired(Action<object?> callback, object? state, LifecycleToken token);
 	void UnregisterOnExpired(LifecycleExpiredRegistration registration, LifecycleToken token);
-	
-	ReactiveLifecycle ReactiveLifecycle { get; }
 }
 
 

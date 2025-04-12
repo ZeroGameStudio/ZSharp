e5bf8fa4a8bd5a5df85fed93fa6351a34f98654e
Lin7Han <941532182@qq.com>
2025-04-12 10:03:21 +0800

    Remove ActorComponent.SetIsReplicatedByDefault().

--- Script/ZeroGames.ZSharp.UnrealEngine/Source/Engine/ActorComponent.cs
+++ Script/ZeroGames.ZSharp.UnrealEngine/Source/Engine/ActorComponent.cs
@@ -16,11 +16,6 @@ public partial class ActorComponent
 		return InternalGetNetMode();
 	}
 
-	public void SetIsReplicatedByDefault(bool replicated)
-	{
-		MasterAlcCache.GuardInvariant();
-	}
-	
 	private unsafe ENetMode InternalGetNetMode()
 		=> ActorComponent_Interop.GetNetMode(ConjugateHandle.FromConjugate(this));
 

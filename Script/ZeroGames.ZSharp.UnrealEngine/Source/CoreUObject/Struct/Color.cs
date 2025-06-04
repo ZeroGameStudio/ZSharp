ba8ba69806d975fa67214f45ff9c6c7fd87f3d32
Ling Han <941532182@qq.com>
2025-06-04 13:58:45 +0800

    Add color APIs.

---
+++ Script/ZeroGames.ZSharp.UnrealEngine/Source/CoreUObject/Struct/Color.cs
@@ -0,0 +1,33 @@
+﻿// Copyright Zero Games. All Rights Reserved.
+
+namespace ZeroGames.ZSharp.UnrealEngine.CoreUObject;
+
+public partial class FColor
+{
+    
+    public FColor(uint8 r, uint8 g, uint8 b, uint8 a = 255)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
+
+    public static FColor White => new(255, 255, 255);
+    public static FColor Black => new(0, 0, 0);
+    public static FColor Transparent => new(0, 0, 0, 0);
+    public static FColor Red => new(255, 0, 0);
+    public static FColor Green => new(0, 255, 0);
+    public static FColor Blue => new(0, 0, 255);
+    public static FColor Cyan => new(0, 255, 255);
+    public static FColor Magenta => new(255, 0, 255);
+    public static FColor Yellow => new(255, 255, 0);
+    public static FColor Orange => new(243, 156, 18);
+    public static FColor Purple => new(169, 7, 228);
+    public static FColor Turquoise => new(26, 188, 156);
+    public static FColor Silver => new(189, 195, 199);
+    public static FColor Emerald => new(46, 204, 113);
+
+}
+
+
---
+++ Script/ZeroGames.ZSharp.UnrealEngine/Source/SlateCore/SlateColor.cs
@@ -0,0 +1,36 @@
+﻿// Copyright Zero Games. All Rights Reserved.
+
+namespace ZeroGames.ZSharp.UnrealEngine.SlateCore;
+
+public partial class FSlateColor
+{
+
+    public FSlateColor(float r, float g, float b, float a = 1)
+    {
+        SpecifiedColor = new(r, g, b, a);
+    }
+
+    public FSlateColor(FLinearColor color)
+    {
+        SpecifiedColor = color;
+    }
+
+    public FSlateColor(FColor color, bool srgb)
+    {
+        SpecifiedColor = new(color, srgb);
+    }
+    
+    public static FSlateColor White => new(1,1,1);
+    public static FSlateColor Gray => new(0.5f,0.5f,0.5f);
+    public static FSlateColor Black => new(0,0,0);
+    public static FSlateColor Transparent => new(0,0,0,0);
+    public static FSlateColor Red => new(1,0,0);
+    public static FSlateColor Green => new(0,1,0);
+    public static FSlateColor Blue => new(0,0,1);
+    public static FSlateColor Cyan => new(0, 1, 1);
+    public static FSlateColor Magenta => new(1, 0, 1);
+    public static FSlateColor Yellow => new(1,1,0);
+    
+}
+
+

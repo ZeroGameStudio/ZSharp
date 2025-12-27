baef6d071308f3b14e8cfd648b2e33ebc78a0039
Ling Han <125175175+Lin7Han@users.noreply.github.com>
2025-12-27 11:51:59 +0800

    AssetHandle.

---
+++ Script/ZeroGames.ZSharp.UnrealEngine/Source/Misc/Asset/AssetHandle.cs
@@ -0,0 +1,55 @@
+﻿// Copyright Zero Games. All Rights Reserved.
+
+namespace ZeroGames.ZSharp.UnrealEngine;
+
+public struct AssetHandle(string? path)
+{
+	public AssetHandle(UObject? asset) : this(asset?.GetPathName()){}
+	
+	public UObject? Asset
+	{
+		get
+		{
+			if (string.IsNullOrEmpty(_path))
+			{
+				return null;
+			}
+			
+			if (field is not { __IsValid: true })
+			{
+				field = LoadObject(UObject.StaticClass, null, _path);
+			}
+			
+			return field?.SelfIfValid;
+		}
+	}
+	
+	private readonly string? _path = path;
+}
+
+public struct AssetHandle<T>(string? path) where T : UObject
+{
+	public AssetHandle(T? asset) : this(asset?.GetPathName()){}
+	
+	public T? Asset
+	{
+		get
+		{
+			if (string.IsNullOrEmpty(_path))
+			{
+				return null;
+			}
+			
+			if (field is not { __IsValid: true })
+			{
+				field = LoadObject<T>(null, _path);
+			}
+			
+			return field?.SelfIfValid;
+		}
+	}
+	
+	private readonly string? _path = path;
+}
+
+
